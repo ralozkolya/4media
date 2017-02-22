ee48e98039a0c5670bc39776f75f184cc1dbd24b
Kolya <raloz1@gmail.com>
2017-02-23 01:17:35 +0400

    Correct typo

--- www/application/views/pages/home.php
+++ www/application/views/pages/home.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo get_lang_code(get_lang()); ?>">
 <head>
 	<?php $this->load->view('elements/head'); ?>
-	<link rel="stylesheet" href="<?php echo static_url('css/nanoscroller.css?v='.V); ?>">
+	<link rel="stylesheet" href="<?php echo static_url('css/nanoScroller.css?v='.V); ?>">
 	<link rel="stylesheet" href="<?php echo static_url('css/home.css?v='.V); ?>">
 	<script src="<?php echo static_url('js/nanoscroller.min.js?V='.V); ?>"></script>
 	<script src="<?php echo static_url('js/home.js?V='.V); ?>"></script>
