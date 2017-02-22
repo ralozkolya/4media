0b6a234ca856dd0b2e07bfa7ca5ff0323b27f31f
Kolya <raloz1@gmail.com>
2017-02-23 00:58:47 +0400

    Change section buttons

--- www/application/views/slides/main.php
+++ www/application/views/slides/main.php
@@ -6,39 +6,33 @@
 	</div>
 	<div class="text-center">
 		<a href="#" class="unstyled open-overlay" data-opens=".sites">
-			<div class="section-button">
-				<div><span class="fa fa-television"></span></div>
+			<div class="section-button make-sites">
 				<div class="name"><?php echo lang('make_sites'); ?></div>
 			</div>
 		</a>
 		<a href="#" class="unstyled">
-			<div class="section-button">
-				<div><span class="fa fa-globe"></span></div>
+			<div class="section-button social-media">
 				<div class="name"><?php echo lang('social_media'); ?></div>
 			</div>
 		</a>
 		<a href="#" class="unstyled">
-			<div class="section-button">
-				<div><span class="fa fa-users"></span></div>
+			<div class="section-button about-us">
 				<div class="name"><?php echo lang('about_us'); ?></div>
 			</div>
 		</a>
 		<br class="hidden-xs">
 		<a href="#" class="unstyled open-overlay" data-opens=".sense">
-			<div class="section-button">
-				<div><span class="fa fa-google"></span></div>
+			<div class="section-button google">
 				<div class="name"><?php echo lang('adwords'); ?></div>
 			</div>
 		</a>
 		<a href="<?php echo locale_url('blog'); ?>" class="unstyled">
-			<div class="section-button">
-				<div><span class="fa fa-bold"></span></div>
+			<div class="section-button blog">
 				<div class="name"><?php echo lang('blog'); ?></div>
 			</div>
 		</a>
 		<a href="#" class="unstyled open-overlay" data-opens=".other">
-			<div class="section-button">
-				<div><span class="fa fa-paper-plane-o"></span></div>
+			<div class="section-button searching-for-other">
 				<div class="name"><?php echo lang('searching_for_other'); ?></div>
 			</div>
 		</a>
