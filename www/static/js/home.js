fc60cf34bbaf79cad36158190574fca8414e5b3d
Kolya <raloz1@gmail.com>
2017-02-28 02:57:42 +0400

    Add 2-column layout for portfolio

--- www/static/js/home.js
+++ www/static/js/home.js
@@ -117,6 +117,13 @@ $(function() {
 			{
 				breakpoint: 767,
 				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 460,
+				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1,
 				}
